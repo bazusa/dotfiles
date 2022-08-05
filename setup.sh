29789e5d00e62194ac33b0cf33de6ad7d771d861
bazusa <bazusa@gmail.com>
2022-08-05 13:53:22 -0600

    add brew check

--- setup.sh
+++ setup.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -x
+IFS=$'\n\t'
+DOTFILES="$HOME/.dotfiles"
 
-echo "Hello"
+main() {
+  echo "${DOTFILES}"
+  if ! [ -x "$(command -v brew)" ]; then
+    echo "first"
+  else
+    echo "last"
+  fi
+  # if ! [ -x "$(command -v brew)" ]; then
+  #   echo "Installing homebrew ..."
+  #   #  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # else
+  #   echo "Homebrew installed ... skipped"
+  # fi
+
+}
+
+main "$@"

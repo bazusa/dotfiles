b17fc8fc956b30281837df997db92551c416d683
bazusa <bazusa@gmail.com>
2023-08-27 14:02:25 -0600

    Create functions.sh

---
+++ sys/extras/functions.sh
@@ -0,0 +1,12 @@
+# my functions
+
+function h() {
+    # check if we passed any parameters
+    if [ -z "$*" ]; then
+        # if no parameters were passed print entire history
+        history 1
+    else
+        # if words were passed use it as a search
+        history 1 | egrep --color=auto "$@"
+    fi
+}

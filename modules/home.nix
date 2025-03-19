348a5d9b28917b6f8b76aaa340d50e0aff89c4bf
bazusa <bazusa@gmail.com>
2025-03-19 16:46:20 -0600

    up

--- modules/home.nix
+++ modules/home.nix
@@ -3,4 +3,13 @@
 {
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      cls = "clear";
+      switch = "darwin-rebuild switch --flake ~/.dotfiles";
+    };
+  };
 }

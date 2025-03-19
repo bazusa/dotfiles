e361d0b7e54f07f9dd76e83a5c8fd4c0142481f3
bazusa <bazusa@gmail.com>
2025-03-19 15:38:28 -0600

    updated

--- flake.nix
+++ flake.nix
@@ -1,23 +1,29 @@
 {
+  description = "Example nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, mac-app-util }:
-  {
-    darwinConfigurations."bron" = nix-darwin.lib.darwinSystem {
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 6;
-      modules = [
-        ./modules/system.nix
-        mac-app-util.darwinModules.default
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      environment.systemPackages =
+        [ pkgs.vim
         ];
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      system.stateVersion = 6;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
     };
   };
 }

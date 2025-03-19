b8ac66a3e1e365242207a5d63a4a68cb3a094498
bazusa <bazusa@gmail.com>
2025-03-19 16:13:33 -0600

    s

--- modules/home.nix
+++ modules/home.nix
@@ -1,6 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  home.username = "bazusa";
+  home.homeDirectory = "/Users/bazusa";
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
 }

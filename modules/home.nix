ba5b0238cbbb67a9d8320917ac80b134ce75646e
bazusa <bazusa@gmail.com>
2025-03-20 07:19:03 -0600

    update

--- modules/home.nix
+++ modules/home.nix
@@ -13,4 +13,27 @@
       switch = "darwin-rebuild switch --flake ~/.dotfiles";
     };
   };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc
+    ];
+    profiles.default.userSettings = {
+      "editor.fontFamily" = "Hack Nerd Font";
+      "editor.fontSize" = 14;
+      "editor.fontLigatures" = true;
+      "editor.formatOnSave" = true;
+      "editor.lineHeight" = 1.4;
+      "editor.minimap.enabled" = false;
+      "editor.renderWhitespace" = "all";
+      "editor.tabSize" = 2;
+      "editor.wordWrap" = "on";
+      "files.autoSave" = "afterDelay";
+      "files.autoSaveDelay" = 1000;
+      "files.trimTrailingWhitespace" = true;
+      "workbench.colorTheme" = "Catppuccino Macchiato";
+    };
+  };
 }
--- modules/system.nix
+++ modules/system.nix
@@ -2,16 +2,28 @@
 
 {
   nix.enable = false;
+  nixpkgs.hostPlatform = "x86_64-darwin";
+  nixpkgs.config.allowUnfree = true;
 
   networking.hostName = "bron";
   networking.localHostName = "bron";
   networking.computerName = "bron";
 
-  nixpkgs.hostPlatform = "x86_64-darwin";
-  nixpkgs.config.allowUnfree = true;
-
   environment.systemPackages = with pkgs; [
     vim
     iterm2
   ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.hack
+  ];
+
+  system.defaults = {
+    dock.autohide = false;
+    dock.persistent-apps = [
+      "${pkgs.iterm2}/Applications/iTerm2.app"
+      "/System/Applications/Mail.app"
+    ];
+  };
+
 }
